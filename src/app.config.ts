ec1e1f97916c4900d5c2e11f37cb1595dabe09d7
huanxlu <huanxlu@163.com>
2023-06-02 17:31:11 +0800

    feat: tabBar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,16 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/noteList/index',
+    'pages/note/index',
+    'pages/noteTrash/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    list: [{pagePath: 'pages/noteList/index', text: '笔记本列表'}, {pagePath: 'pages/note/index', text: '笔记'},{pagePath: 'pages/noteTrash/index', text: '回收站'}]
   }
 })
